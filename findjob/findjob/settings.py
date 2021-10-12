ceb788a2325ae1a28adb1593b9f95f2ada212656
Admin <trao0312@gmail.com>
2021-10-12 17:52:08 +0700

    findjob

--- findjob/findjob/settings.py
+++ findjob/findjob/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vj!lgx)$9k*vtp5s)pm&fqe8yn7i_mdy=n-bmy3l#relsyc6j9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://findjobsweb.herokuapp.com']
 
 
 # Application definition
@@ -130,10 +130,21 @@ WSGI_APPLICATION = 'findjob.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'findjobs',
+        'USER': 'postgres',
+        'PASSWORD': '29091999trao*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -192,3 +203,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if os.getcwd() == '/app':
+    DEBUG = False
